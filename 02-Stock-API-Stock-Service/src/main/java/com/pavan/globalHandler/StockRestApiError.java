a73218ea9be8b42c6e1c22c77d4df1f1e843d487
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 05:19:23 +0530

    Stock Api Microservice for the Service Layer

--- 02-Stock-API-Stock-Service/src/main/java/com/pavan/globalHandler/StockRestApiError.java
+++ 02-Stock-API-Stock-Service/src/main/java/com/pavan/globalHandler/StockRestApiError.java
@@ -3,9 +3,11 @@ package com.pavan.globalHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.pavan.exception.StockNotFoundException;
 
+@RestControllerAdvice
 public class StockRestApiError {
 	
 	@ExceptionHandler(StockNotFoundException.class)
