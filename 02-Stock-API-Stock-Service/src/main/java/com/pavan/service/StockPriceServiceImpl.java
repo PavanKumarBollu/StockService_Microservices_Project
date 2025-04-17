78f3acca206215dfa6bdf355b20ae99b5405e3bb
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 05:28:14 +0530

    Stock Api Microservice for the Service Layer

--- 02-Stock-API-Stock-Service/src/main/java/com/pavan/service/StockPriceServiceImpl.java
+++ 02-Stock-API-Stock-Service/src/main/java/com/pavan/service/StockPriceServiceImpl.java
@@ -3,6 +3,8 @@ package com.pavan.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pavan.entity.StockPrice;
+import com.pavan.exception.StockNotFoundException;
 import com.pavan.repository.IStockRepository;
 
 @Service(value="service")
@@ -13,6 +15,12 @@ public class StockPriceServiceImpl implements IStockPriceService {
 	
 	@Override
 	public Double findByCompanyName(String companyName) {
+		StockPrice stockPrice = repo.findByCompanyName(companyName);
+		if(stockPrice == null)
+		{
+			throw new StockNotFoundException("Company Not Available With " + companyName + " Name");
+		}
+		
 		return null;
 	}
 
