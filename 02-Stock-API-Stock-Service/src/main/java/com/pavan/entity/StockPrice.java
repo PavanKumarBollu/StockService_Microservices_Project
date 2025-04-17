977ecd804b8fc4bbb1e66168baf9f9037df9df0f
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 05:11:04 +0530

    Stock Api Microservice for the Service Layer

--- 02-Stock-API-Stock-Service/src/main/java/com/pavan/entity/StockPrice.java
+++ 02-Stock-API-Stock-Service/src/main/java/com/pavan/entity/StockPrice.java
@@ -1,5 +1,17 @@
 package com.pavan.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "STOCK_PRICE")
 public class StockPrice {
 
+	@Id
+	private Integer stockId;
+	private String companyName;
+	private Double companyPrice;
 }
