d0b3b9b82993359815305d23e4146bf90ddd2e85
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 05:13:54 +0530

    Stock Api Microservice for the Service Layer

--- 02-Stock-API-Stock-Service/src/main/java/com/pavan/exception/StockNotFoundException.java
+++ 02-Stock-API-Stock-Service/src/main/java/com/pavan/exception/StockNotFoundException.java
@@ -2,9 +2,13 @@ package com.pavan.exception;
 
 public class StockNotFoundException extends RuntimeException {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
+
+	public StockNotFoundException(String message) {
+		super(message);
+	}
+	
+	
 
 }
