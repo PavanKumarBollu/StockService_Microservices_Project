c7041774ed063dd2b4868238712334d42a4d14a7
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 05:24:57 +0530

    Stock Api Microservice for the Service Layer

--- 02-Stock-API-Stock-Service/src/main/java/com/pavan/service/StockPriceServiceImpl.java
+++ 02-Stock-API-Stock-Service/src/main/java/com/pavan/service/StockPriceServiceImpl.java
@@ -1,10 +1,12 @@
 package com.pavan.service;
 
+import org.springframework.stereotype.Service;
+
+@Service(value="service")
 public class StockPriceServiceImpl implements IStockPriceService {
 
 	@Override
 	public Double findByCompanyName(String companyName) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
