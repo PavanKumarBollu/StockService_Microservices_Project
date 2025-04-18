7a1337f06df5064cd5f8e48dae522ddb1f1e69e4
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 23:35:26 +0530

    Stock api Project API GateWay Using the Zuul Proxy Server

--- 05-Stock-API-FronEnt-Ui-Application/src/main/java/com/pavan/service/StockService.java
+++ 05-Stock-API-FronEnt-Ui-Application/src/main/java/com/pavan/service/StockService.java
@@ -1,8 +1,20 @@
 package com.pavan.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service(value="service")
 public class StockService {
 
+	
+
+	public String getTotalStockPrice(String compnayName, Integer quantity)
+	{
+		final String REST_ENT_POINT = "http://nobody:3333/api/calc/calc/calculate/{companyName}/{";
+		WebClient client = WebClient.create();
+		client.get().uri(REST_ENT_POINT,compnayName,quantity)
+		return null;
+	}
+	
+	
 }
