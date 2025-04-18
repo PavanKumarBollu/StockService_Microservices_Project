4655fedbef621c383b3577a1126efe5d9f610854
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 23:39:35 +0530

    Stock api Project API GateWay Using the Zuul Proxy Server

--- 05-Stock-API-FronEnt-Ui-Application/src/main/java/com/pavan/controller/StockUiController.java
+++ 05-Stock-API-FronEnt-Ui-Application/src/main/java/com/pavan/controller/StockUiController.java
@@ -1,5 +1,22 @@
 package com.pavan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.pavan.service.StockService;
+
+@Controller
 public class StockUiController {
+	
+	
+	@Autowired
+	private StockService service;
+	
+	@GetMapping("/")
+	public String loadPage()
+	{
+		return "index";
+	}
 
 }
