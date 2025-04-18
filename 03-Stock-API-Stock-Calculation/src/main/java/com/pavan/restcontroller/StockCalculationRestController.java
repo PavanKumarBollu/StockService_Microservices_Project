f980af98da5d349c352af1e44f08e9138b8a31e6
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 22:35:06 +0530

    Stock api Project API GateWay Using the Zuul Proxy Server

--- 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/restcontroller/StockCalculationRestController.java
+++ 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/restcontroller/StockCalculationRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pavan.client.StockClient;
 
 @RestController
-@RequestMapping(value = "/calc")
+@RequestMapping(value = "/cal")
 public class StockCalculationRestController {
 
 	@Autowired
