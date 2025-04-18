7681190ddf01a20fea786140f9620fbdaf2ae121
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 11:36:51 +0530

    Stock Api Microservice for the Stock Calculation to interservice Communication using the feign client

--- 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/restcontroller/StockCalculationRestController.java
+++ 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/restcontroller/StockCalculationRestController.java
@@ -1,18 +1,25 @@
 package com.pavan.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pavan.client.StockClient;
+
 @RestController
 @RequestMapping(value="/calc")
 public class StockCalculationRestController {
+	
+	@Autowired
+	private StockClient client;
 
 	@GetMapping(value="/calculate/{companyName}/{quantity}")
 	public ResponseEntity<?> caluculate(@PathVariable String companyName, @PathVariable Integer quantity)
 	{
+		
 		return null;
 	}
 	
