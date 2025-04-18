a7fdd81ebbcf3cc6778546db90070fcc3d9c2007
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 23:38:32 +0530

    Stock api Project API GateWay Using the Zuul Proxy Server

--- 05-Stock-API-FronEnt-Ui-Application/src/main/java/com/pavan/service/StockService.java
+++ 05-Stock-API-FronEnt-Ui-Application/src/main/java/com/pavan/service/StockService.java
@@ -3,18 +3,20 @@ package com.pavan.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Service(value="service")
+@Service(value = "service")
 public class StockService {
 
-	
-
-	public String getTotalStockPrice(String compnayName, Integer quantity)
-	{
-		final String REST_ENT_POINT = "http://nobody:3333/api/calc/calc/calculate/{companyName}/{";
+	public String getTotalStockPrice(String compnayName, Integer quantity) {
+		final String REST_ENT_POINT = "http://nobody:3333/api/calc/calc/calculate/{companyName}/{quantity}";
+		String result = null;
 		WebClient client = WebClient.create();
-		String result = client.get().uri(REST_ENT_POINT,compnayName,quantity).retrieve().bodyToMono(String.class).block();
+		try {
+			result = client.get().uri(REST_ENT_POINT, compnayName, quantity).retrieve().bodyToMono(String.class)
+					.block();
+		} catch (Exception e) {
+			result="Company not found";
+		}
 		return result;
 	}
-	
-	
+
 }
