c2f2c0b98e91d92c9e5f1702d32d07bf849c1611
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 11:48:52 +0530

    Stock Api Microservice for the Stock Calculation to interservice Communication using the feign client

--- 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/restcontroller/StockCalculationRestController.java
+++ 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/restcontroller/StockCalculationRestController.java
@@ -21,15 +21,21 @@ public class StockCalculationRestController {
 	public ResponseEntity<?> caluculate(@PathVariable String companyName, @PathVariable Integer quantity)
 	{
 		ResponseEntity<?> response;
-		Double stockPrice;
-		response = client.getStockPrice(companyName);
-		int statusCode = response.getStatusCode().value();
-		if(statusCode == 200)
-		{
-			
+		Double totalStockPrice;
+		try {
+			response = client.getStockPrice(companyName);
+			int statusCode = response.getStatusCode().value();
+			if(statusCode == 200)
+			{
+				Double stockPrice  = (Double) response.getBody();
+				totalStockPrice = quantity * stockPrice;
+				
+				String result = "Total Stock Price of : " + companyName + " is : " + totalStockPrice; 
+			}
+		} catch (Exception e) {
 		}
 		
-		return null;
+		return response;
 	}
 	
 }
