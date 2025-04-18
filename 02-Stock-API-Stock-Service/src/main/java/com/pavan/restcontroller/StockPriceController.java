965de4ea436b98ced64c45aadb010f164e047f41
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 05:30:19 +0530

    Stock Api Microservice for the Service Layer

--- 02-Stock-API-Stock-Service/src/main/java/com/pavan/restcontroller/StockPriceController.java
+++ 02-Stock-API-Stock-Service/src/main/java/com/pavan/restcontroller/StockPriceController.java
@@ -1,5 +1,10 @@
 package com.pavan.restcontroller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/price")
 public class StockPriceController {
 
 }
