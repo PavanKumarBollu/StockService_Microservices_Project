082b58c724e9b1c0390f1f8367c575b8855b34cf
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 11:31:09 +0530

    Stock Api Microservice for the Stock Calculation to interservice Communication using the feign client

--- 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/client/StockClient.java
+++ 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/client/StockClient.java
@@ -1,11 +1,12 @@
 package com.pavan.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 
 @FeignClient(name = "02-Stock-API-Stock-Service")
 public interface StockClient {
 
-	
+	public ResponseEntity<String> getStockPrice(String company);
 	
 	
 }
