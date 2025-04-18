6a47aaab034f1ee8a76c031812e12ec5bd4c65fc
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 11:53:00 +0530

    Stock Api Microservice for the Stock Calculation to interservice Communication using the feign client

--- 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/restcontroller/StockCalculationRestController.java
+++ 03-Stock-API-Stock-Calculation/src/main/java/com/pavan/restcontroller/StockCalculationRestController.java
@@ -1,6 +1,7 @@
 package com.pavan.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,32 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pavan.client.StockClient;
 
 @RestController
-@RequestMapping(value="/calc")
+@RequestMapping(value = "/calc")
 public class StockCalculationRestController {
-	
+
 	@Autowired
 	private StockClient client;
-	
 
-	@GetMapping(value="/calculate/{companyName}/{quantity}")
-	public ResponseEntity<?> caluculate(@PathVariable String companyName, @PathVariable Integer quantity)
-	{
+	@GetMapping(value = "/calculate/{companyName}/{quantity}")
+	public ResponseEntity<?> caluculate(@PathVariable String companyName, @PathVariable Integer quantity) {
 		ResponseEntity<?> response;
 		Double totalStockPrice;
 		try {
 			response = client.getStockPrice(companyName);
 			int statusCode = response.getStatusCode().value();
-			if(statusCode == 200)
-			{
-				Double stockPrice  = (Double) response.getBody();
+			if (statusCode == 200) {
+				Double stockPrice = (Double) response.getBody();
 				totalStockPrice = quantity * stockPrice;
-				
-				String result = "Total Stock Price of : " + companyName + " is : " + totalStockPrice; 
+
+				String result = "Total Stock Price of : " + companyName + " is : " + totalStockPrice;
+				response = new ResponseEntity<String>(result, HttpStatus.OK);
 			}
 		} catch (Exception e) {
+			response = new ResponseEntity<String>("Company Not Found", HttpStatus.BAD_REQUEST);
 		}
-		
+
 		return response;
 	}
-	
+
 }
