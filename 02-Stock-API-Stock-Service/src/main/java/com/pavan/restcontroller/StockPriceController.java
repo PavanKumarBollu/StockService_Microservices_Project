235940fce8c7f1b40de0988358a6140d7c9970af
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-18 10:27:28 +0530

    Stock Api Microservice for the Service Layer

--- 02-Stock-API-Stock-Service/src/main/java/com/pavan/restcontroller/StockPriceController.java
+++ 02-Stock-API-Stock-Service/src/main/java/com/pavan/restcontroller/StockPriceController.java
@@ -1,6 +1,8 @@
 package com.pavan.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,13 @@ public class StockPriceController {
 	
 	@Autowired
 	private IStockPriceService service;
+	
+	public ResponseEntity<Double> getStockPrice(String companyName)
+	{
+		Double price = service.findByCompanyName(companyName);
+		System.out.println("company name and price + " + companyName + price);
+		return new ResponseEntity<Double> (price, HttpStatus.OK);
+	}
+	
 
 }
